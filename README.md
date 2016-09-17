4895945ac3e9b8393492e0f21102d8000ca455e8
David Figatner <david@yopeyopey.com>
2016-09-17 15:18:14 +0800

    Updated README; changed to class/ES6 format

--- README.md
+++ README.md
@@ -3,20 +3,18 @@ update loop API for javascript apps. Works well with https://github.com/davidfig
 
 ## Code Example
 
-        // initialize debug panels to track progress
-        Debug.init();
-        var random = Debug.add('random', {text: '0'});
+        const Update = require('@yy/update');
 
         // initialize update loop
         Update.init();
 
         // add a call to testRandom every 100 MS and track it in Random Numbers debug panel
-        Update.add(testRandom, {time: 100, percent: 'Random Numbers'});
+        Update.add(testRandom, {time: 100});
 
-        // add a call to testPI every update and track it in PI Calculation debug panel
-        Update.add(testPI, {percent: 'PI Calculation'});
+        // add a call to testPI every update
+        Update.add(testPI);
 
-        // add a call to testDelay (tracked automatically in Other debug panel)
+        // add a call to testDelay
         Update.add(testDelay);
 
         // start update loop
@@ -41,50 +39,163 @@ update loop API for javascript apps. Works well with https://github.com/davidfig
             }
         }
 
-## Installation
-include update.js in your project or add to your workflow
-
-    <script src="update.js"></script>
-
-## Example
+## Live Example
 https://davidfig.github.io/update/
 
 see also
 
-* https://davidfig.github.io/debug/
 * https://davidfig.github.io/animate/
 * https://davidfig.github.io/renderer/
 * https://davidfig.github.io/viewport/
 
-## API Reference
+## Installation
+include update.js in your project or add to your workflow
 
-### Update.init()
-initialize Update
+    npm install --save davidfig/update
 
-### Update.update()
+# API Reference
+**Kind**: global class  
+
+* [Update](#Update)
+    * [.init([Debug], [count], [percent], [FPS])](#Update+init)
+    * [.registerPause(pause, resume)](#Update+registerPause)
+    * [.pauseGame()](#Update+pauseGame)
+    * [.resumeGame()](#Update+resumeGame)
+    * [._updateAll(elapsed)](#Update+_updateAll)
+    * [.add(funct, options)](#Update+add)
+    * [.remove(update)](#Update+remove)
+    * [.update()](#Update+update)
+    * [.checkVisibility()](#Update+checkVisibility)
+    * [.visibilityChange()](#Update+visibilityChange)
+    * [._debugInit()](#Update+_debugInit)
+    * [._debugUpdate(current)](#Update+_debugUpdate)
+    * [._debugPercent(other)](#Update+_debugPercent)
+
+<a name="Update+init"></a>
+
+### update.init([Debug], [count], [percent], [FPS])
+must call this.init before using Update
+
+**Kind**: instance method of <code>[Update](#Update)</code>  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| [Debug] | <code>object</code> | pass Debug from github.com/davidfig/debug |
+| [count] | <code>boolean</code> &#124; <code>string</code> | show debug counts (can supply side for panel, e.g., 'topleft') |
+| [percent] | <code>boolean</code> &#124; <code>string</code> | show debug percentage |
+| [FPS] | <code>boolean</code> &#124; <code>string</code> | show debug FPS |
+
+<a name="Update+registerPause"></a>
+
+### update.registerPause(pause, resume)
+register functions to call after Update pauses or resumes
+
+**Kind**: instance method of <code>[Update](#Update)</code>  
+
+| Param | Type |
+| --- | --- |
+| pause | <code>function</code> | 
+| resume | <code>function</code> | 
+
+<a name="Update+pauseGame"></a>
+
+### update.pauseGame()
+pauses all updates
+
+**Kind**: instance method of <code>[Update](#Update)</code>  
+<a name="Update+resumeGame"></a>
+
+### update.resumeGame()
+resumes all updates
+
+**Kind**: instance method of <code>[Update](#Update)</code>  
+<a name="Update+_updateAll"></a>
+
+### update._updateAll(elapsed)
+loops through all updates
+
+**Kind**: instance method of <code>[Update](#Update)</code>  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| elapsed | <code>number</code> | in milliseconds |
+
+<a name="Update+add"></a>
+
+### update.add(funct, options)
+adds a function to the update loop
+
+**Kind**: instance method of <code>[Update](#Update)</code>  
+
+| Param | Type | Default | Description |
+| --- | --- | --- | --- |
+| funct | <code>function</code> |  |  |
+| options | <code>object</code> |  |  |
+| [options.time] | <code>number</code> | <code>0</code> | in milliseconds to call this function |
+| [options.FPS] | <code>number</code> |  | this replaces options.time and calls the function at the desired FPS |
+| [options.once] | <code>boolean</code> | <code>false</code> | call only once and then remove from update queue |
+| [options.percent] | <code>string</code> |  | name to track the percentage in the debug panel |
+
+<a name="Update+remove"></a>
+
+### update.remove(update)
+removes an update from the loop
+
+**Kind**: instance method of <code>[Update](#Update)</code>  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| update | <code>object</code> | object returned by this.add() |
+
+<a name="Update+update"></a>
+
+### update.update()
 starts the update loop
 
-### Update.add(funct, options)
-Add a function to the update loop
-* funct: function pointer
-* options {}
-    - options.time: time to execute funct in MS (defaults to every update loop)
-    - options.FPS: desired frames per second to call funct
-    - options.once: true or false -- call function only once
-    - percent: track percentages in a davidfig/Debug panel
+**Kind**: instance method of <code>[Update](#Update)</code>  
+<a name="Update+checkVisibility"></a>
 
-### Update.remove(update)
-removes an update from the loop
-* update: object returned by Update.add() or Update.addFPS()
+### update.checkVisibility()
+Starts a page visibility event listener running, or window.onpagehide/onpageshow if not supported by the browser. Also listens for window.onblur and window.onfocus.
+based on Phaser: https://github.com/photonstorm/phaser (MIT license)
 
-### Update.registerPause(pause, resume)
-registers callback for pause and resume
+**Kind**: instance method of <code>[Update](#Update)</code>  
+<a name="Update+visibilityChange"></a>
 
-### Update.pauseGame()
-pauses the update loop -- this is automatically called when document loses focus
+### update.visibilityChange()
+Callback for checkVisibility()
+based on Phaser: https://github.com/photonstorm/phaser (MIT license)
 
-### Update.resumeGame()
-resumes the update loop -- this is automatically called when document gains focus
+**Kind**: instance method of <code>[Update](#Update)</code>  
+<a name="Update+_debugInit"></a>
 
-## License
-MIT License (MIT)
+### update._debugInit()
+adds debug panels (uses github.com/davidfig/debug)
+
+**Kind**: instance method of <code>[Update](#Update)</code>  
+<a name="Update+_debugUpdate"></a>
+
+### update._debugUpdate(current)
+update debug panels
+
+**Kind**: instance method of <code>[Update](#Update)</code>  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| current | <code>number</code> | time |
+
+<a name="Update+_debugPercent"></a>
+
+### update._debugPercent(other)
+update percentage panel
+
+**Kind**: instance method of <code>[Update](#Update)</code>  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| other | <code>number</code> | time |
+
+
+* * *
+
+Copyright (c) 2016 YOPEY YOPEY LLC - MIT License - Documented by [jsdoc-to-markdown](https://github.com/75lb/jsdoc-to-markdown)
--- update.js
+++ update.js
@@ -5,117 +5,124 @@
     Copyright (c) 2016 YOPEY YOPEY LLC
 */
 
-/* globals Debug, debugOne, performance, requestAnimationFrame, setTimeout, window, define, document */
-var Update = {
+let Debug;
 
-    list: [],
-    FPS: 60,
-    pause: false,
-    pauseRegistrations: [],
-    pauseElapsed: 0,
+/** @class */
+class Update
+{
+    constructor()
+    {
+        this.list = [];
+        this.FPS = 60;
+        this.pause = false;
+        this.pauseRegistrations = [];
+        this.pauseElapsed = 0;
 
-    startTime: 0,
-    frameNumber: 0,
-    lastUpdate: 0,
-    lastFPS: '--',
+        this.startTime = 0;
+        this.frameNumber = 0;
+        this.lastUpdate = 0;
+        this.lastFPS = '--';
 
-    tolerance: 1,
+        this.tolerance = 1;
 
-    maxChange: 100,
+        this.maxChange = 100;
 
-    panels: {fps: null, meter: null, percent: null},
-    percentageList: [],
+        this.panels = {fps: null, meter: null, percent: null};
+        this.percentageList = [];
 
-    // this is the number of entries to use in a rolling average to smooth the debug percentages
-    rollingAverage: 500,
+        // this is the number of entries to use in a rolling average to smooth the debug percentages
+        this.rollingAverage = 500;
+    }
 
     /**
-     * must call Update.init before using Update
-     * @param {boolean} debug - whether to turn on debug support (requires github.com/davidfig/debug)
-     * @param {boolean=false|string} count - show debug counts (can supply side for panel, e.g., 'topleft')
-     * @param {boolean=false|string} percent - show debug percentage
-     * @param {boolean=false|string} FPS - show debug FPS
+     * must call this.init before using Update
+     * @param {object} [Debug] pass Debug from github.com/davidfig/debug
+     * @param {boolean|string} [count] - show debug counts (can supply side for panel, e.g., 'topleft')
+     * @param {boolean|string} [percent] - show debug percentage
+     * @param {boolean|string} [FPS] - show debug FPS
      */
-    init: function(options)
+    init(options)
     {
         options = options || {};
-        Update.checkVisibility();
-        Update.FPSActual = 1000 / Update.FPS;
+        this.checkVisibility();
+        this.FPSActual = 1000 / this.FPS;
         if (options.debug)
         {
-            Update.debug = {count: options.count, percent: options.percent, FPS: options.FPS};
-            Update._debugInit();
+            Debug = options.debug;
+            this.debug = {count: options.count, percent: options.percent, FPS: options.FPS};
+            this._debugInit();
         }
-    },
+    }
 
     /**
      * register functions to call after Update pauses or resumes
      * @param {Function} pause
      * @param {Function} resume
      */
-    registerPause: function(pause, resume)
+    registerPause(pause, resume)
     {
-        Update.pauseRegistrations.push({ pause: pause, resume: resume});
-    },
+        this.pauseRegistrations.push({ pause: pause, resume: resume});
+    }
 
     /**
      * pauses all updates
      */
-    pauseGame: function()
+    pauseGame()
     {
-        if (Update.pause !== true)
+        if (this.pause !== true)
         {
-            Update.pause = true;
-            Update.pauseElapsed = performance.now() - Update.lastUpdate;
-            Update.updateOff = false;
-            if (Update.debug)
+            this.pause = true;
+            this.pauseElapsed = performance.now() - this.lastUpdate;
+            this.updateOff = false;
+            if (this.debug)
             {
-                Update._debugPause();
+                this._debugPause();
             }
-            for (var i = 0; i < Update.pauseRegistrations.length; i++)
+            for (var i = 0; i < this.pauseRegistrations.length; i++)
             {
-                Update.pauseRegistrations[i].pause();
+                this.pauseRegistrations[i].pause();
             }
         }
-    },
+    }
 
     /**
      * resumes all updates
      */
-    resumeGame: function()
+    resumeGame()
     {
-        if (Update.pause === true)
+        if (this.pause === true)
         {
-            Update.pause = false;
-            Update.lastUpdate = performance.now() - Update.pauseElapsed;
-            if (Update.debug && Update.debug.FPS)
+            this.pause = false;
+            this.lastUpdate = performance.now() - this.pauseElapsed;
+            if (Debug && this.debug && this.debug.FPS)
             {
-                Update.startTime = 0;
-                Update.frameNumber = 0;
-                Update.lastUpdate = 0;
-                Update.lastFPS = '--';
+                this.startTime = 0;
+                this.frameNumber = 0;
+                this.lastUpdate = 0;
+                this.lastFPS = '--';
             }
-            for (var i = 0; i < Update.pauseRegistrations.length; i++)
+            for (var i = 0; i < this.pauseRegistrations.length; i++)
             {
-                Update.pauseRegistrations[i].resume();
+                this.pauseRegistrations[i].resume();
             }
-            if (Update.updateOff === true)
+            if (this.updateOff === true)
             {
-                Update.update();
+                this.update();
             }
         }
-    },
+    }
 
     /**
      * loops through all updates
+     * @param {number} elapsed in milliseconds
      */
-    _updateAll: function(elapsed)
+    _updateAll(elapsed)
     {
-        var i = 0, _i = Update.list.length;
+        var i = 0, _i = this.list.length;
         var other = 0, count = 0;
         while (i < _i)
         {
-            var update = Update.list[i++];
+            var update = this.list[i++];
             if (update.pause)
             {
                 continue;
@@ -125,11 +132,11 @@ var Update = {
                 update.elapsed += elapsed;
                 if (update.elapsed < update.duration)
                 {
-                    if (Update.debug.percent && update.options.percent)
+                    if (this.debug.percent && update.options.percent)
                     {
-                        var change = Update.percentageList[update.options.percent];
+                        var change = this.percentageList[update.options.percent];
                         change.amounts[change.current++] = 0;
-                        if (change.current === Update.rollingAverage)
+                        if (change.current === this.rollingAverage)
                         {
                             change.current = 0;
                         }
@@ -142,16 +149,16 @@ var Update = {
                 }
             }
             var start, result;
-            if (Update.debug && Update.debug.percent)
+            if (this.debug && this.debug.percent)
             {
                 start = performance.now();
                 result = update.callback(elapsed, update.options);
                 var current = performance.now() - start;
                 if (update.options.percent)
                 {
-                    var change = Update.percentageList[update.options.percent];
+                    var change = this.percentageList[update.options.percent];
                     change.amounts[change.current++] = current;
-                    if (change.current === Update.rollingAverage)
+                    if (change.current === this.rollingAverage)
                     {
                         change.current = 0;
                     }
@@ -169,111 +176,111 @@ var Update = {
             {
                 i--;
                 _i--;
-                Update.list.splice(i, 1);
+                this.list.splice(i, 1);
             }
             count++;
         }
-        if (Update.debug)
+        if (this.debug)
         {
-            if (Update.debug.count && Update.lastCount !== count)
+            if (this.debug.count && this.lastCount !== count)
             {
-                debugOne(count + ' updates', {panel: Update.panels.count});
-                Update.lastCount = count;
+                Debug.one(count + ' updates', {panel: this.panels.count});
+                this.lastCount = count;
             }
-            if (Update.debug.percent)
+            if (this.debug.percent)
             {
-                Update._debugPercent(other);
+                this._debugPercent(other);
             }
         }
-    },
+    }
 
     /**
      * adds a function to the update loop
      * @param {Function} funct
      * @param {object} options
-     * @param {number=0} options.time in milliseconds to call this function
-     * @param {number=} options.FPS - this replaces options.time and calls the function at the desired FPS
-     * @param {boolean=false} options.once - call only once and then remove from update queue
-     * @param {string=} options.percent - name to track the percentage in the debug panel
+     * @param {number} [options.time=0] in milliseconds to call this function
+     * @param {number} [options.FPS] - this replaces options.time and calls the function at the desired FPS
+     * @param {boolean} [options.once=false] - call only once and then remove from update queue
+     * @param {string} [options.percent] - name to track the percentage in the debug panel
      */
-    add: function(funct, options)
+    add(funct, options)
     {
         options = options || {};
         var time = options.time || (options.FPS ? 1000 / options.FPS : 0);
         var update = {callback: funct, options: options, duration: time, elapsed: 0, once: options.once, pause: false};
-        Update.list.push(update);
-        if (Update.debug && options.percent)
+        this.list.push(update);
+        if (this.debug && options.percent)
         {
-            Update.percentageList[options.percent] = {current: 0, amounts: []};
+            this.percentageList[options.percent] = {current: 0, amounts: []};
             var test = '';
-            for (var key in Update.percentageList)
+            for (var key in this.percentageList)
             {
                 test += key + ': --%<br>';
             }
-            debugOne(test, {panel: Update.panels.percent});
+            Debug.one(test, {panel: this.panels.percent});
             Debug.resize();
         }
         return update;
-    },
+    }
 
     /**
      * removes an update from the loop
-     * @param {object} update - object returned by Update.add()
+     * @param {object} update - object returned by this.add()
      */
-    remove: function(update)
+    remove(update)
     {
-        var index = Update.list.indexOf(update);
-        Update.list.splice(index, 1);
-    },
+        var index = this.list.indexOf(update);
+        this.list.splice(index, 1);
+    }
 
     /**
      * starts the update loop
      */
-    update: function()
+    update()
     {
-        if (Update.pause === true)
+        if (this.pause === true)
         {
-            Update.updateOff = true;
+            this.updateOff = true;
             return;
         }
         var current = performance.now();
         var elapsed;
-        if (Update.lastUpdate === 0)
+        if (this.lastUpdate === 0)
         {
             elapsed = 0;
         }
         else
         {
-            elapsed = current - Update.lastUpdate;
-            elapsed = elapsed > Update.maxChange ? Update.maxChange : elapsed;
+            elapsed = current - this.lastUpdate;
+            elapsed = elapsed > this.maxChange ? this.maxChange : elapsed;
         }
-        if (Update.FPS === 60 || elapsed === 0 || elapsed >= Update.FPSActual)
+        if (this.FPS === 60 || elapsed === 0 || elapsed >= this.FPSActual)
         {
-            if (Update.list.length)
+            if (this.list.length)
             {
-                Update._updateAll(elapsed);
+                this._updateAll(elapsed);
             }
-            Update.lastUpdate = current;
-            if (Update.debug && Update.debug.FPS)
+            this.lastUpdate = current;
+            if (this.debug && this.debug.FPS)
             {
-                Update._debugUpdate(current);
+                this._debugUpdate(current);
             }
         }
         if (typeof requestAnimationFrame === 'function')
         {
-            requestAnimationFrame(Update.update);
+            requestAnimationFrame(this.update.bind(this));
         }
         else
         {
-            setTimeout(Update.update, Update.FPSActual);
+            setTimeout(this.update.bind(this), this.FPSActual);
         }
-    },
+    }
 
     /**
      * Starts a page visibility event listener running, or window.onpagehide/onpageshow if not supported by the browser. Also listens for window.onblur and window.onfocus.
      * based on Phaser: https://github.com/photonstorm/phaser (MIT license)
      */
-    checkVisibility: function()
+    checkVisibility()
     {
         var hiddenVar;
         if (document.webkitHidden !== undefined)
@@ -300,109 +307,114 @@ var Update = {
         //  Does browser support it? If not (like in IE9 or old Android) we need to fall back to blur/focus
         if (hiddenVar)
         {
-            document.addEventListener(hiddenVar, Update.visibilityChange, false);
+            document.addEventListener(hiddenVar, this.visibilityChange.bind(this), false);
         }
 
-        window.onblur = Update.visibilityChange;
-        window.onfocus = Update.visibilityChange;
+        window.onblur = this.visibilityChange.bind(this);
+        window.onfocus = this.visibilityChange.bind(this);
 
-        window.onpagehide = Update.visibilityChange;
-        window.onpageshow = Update.visibilityChange;
-    },
+        window.onpagehide = this.visibilityChange.bind(this);
+        window.onpageshow = this.visibilityChange.bind(this);
+    }
 
     /**
      * Callback for checkVisibility()
      * based on Phaser: https://github.com/photonstorm/phaser (MIT license)
      */
-    visibilityChange: function(event)
+    visibilityChange(event)
     {
         if (event.type === 'pagehide' || event.type === 'blur' || event.type === 'pageshow' || event.type === 'focus')
         {
             if (event.type === 'pagehide' || event.type === 'blur')
             {
-                Update.pauseGame();
+                this.pauseGame();
             }
             else if (event.type === 'pageshow' || event.type === 'focus')
             {
-                Update.resumeGame(event);
+                this.resumeGame(event);
             }
             return;
         }
 
         if (document.hidden || document.mozHidden || document.msHidden || document.webkitHidden || event.type === 'pause')
         {
-            Update.pauseGame(event);
+            this.pauseGame(event);
         }
-    },
+    }
 
     /**
      * adds debug panels (uses github.com/davidfig/debug)
      */
-    _debugInit: function()
+    _debugInit()
     {
-        if (Update.debug.FPS)
+        if (this.debug.FPS)
         {
-            Update.panels.fps = Debug.add('FPS', {text: '-- FPS'});
-            Update.panels.meter = Debug.addMeter('panel');
+            this.panels.fps = Debug.add('FPS', {text: '-- FPS'});
+            this.panels.meter = Debug.addMeter('panel');
         }
-        if (Update.debug.count)
+        if (this.debug.count)
         {
-            Update.panels.count = Debug.add('Updates', {text: '0 updates'});
+            this.panels.count = Debug.add('Updates', {text: '0 updates'});
         }
-        if (Update.debug.percent)
+        if (this.debug.percent)
         {
-            Update.panels.percent = Debug.add('percentages', {style: {textAlign: 'right'}});
-            Update.percentageList['Other'] = {current: 0, amounts: []};
+            this.panels.percent = Debug.add('percentages', {style: {textAlign: 'right'}});
+            this.percentageList['Other'] = {current: 0, amounts: []};
         }
-    },
+    }
 
-    _debugPause: function()
+    _debugPause()
     {
-        if (Debug.debug.FPS)
+        if (this.debug.FPS)
         {
-            debugOne('-- FPS', {panel: Update.panels.fps});
+            Debug.one('-- FPS', {panel: this.panels.fps});
         }
-    },
+    }
 
-    _debugUpdate: function(current)
+    /**
+     * update debug panels
+     * @param {number} current time
+     */
+    _debugUpdate(current)
     {
-        Update.frameNumber++;
-        var currentTime = performance.now() - Update.startTime;
+        this.frameNumber++;
+        var currentTime = performance.now() - this.startTime;
 
         // skip the first half second to get rid of garbage
         if (currentTime > 500)
         {
-            if (Update.startTime !== 0)
+            if (this.startTime !== 0)
             {
-                Update.lastFPS = Math.floor(Update.frameNumber / (currentTime / 1000));
-                if (Update.lastFPS >= 60 - Update.tolerance && Update.lastFPS <= 60 + Update.tolerance)
+                this.lastFPS = Math.floor(this.frameNumber / (currentTime / 1000));
+                if (this.lastFPS >= 60 - this.tolerance && this.lastFPS <= 60 + this.tolerance)
                 {
-                    Update.lastFPS = 60;
+                    this.lastFPS = 60;
                 }
             }
-            Update.startTime = performance.now();
-            Update.frameNumber = 0;
+            this.startTime = performance.now();
+            this.frameNumber = 0;
         }
-        debugOne(Update.lastFPS + ' FPS', {panel: Update.panels.fps});
+        Debug.one(this.lastFPS + ' FPS', {panel: this.panels.fps});
         var time = performance.now() - current;
 
         // 16.7 is hard coded for 60 FPS. Probably should figure out the real amount for the current FPS
         var expected = (16.7 - time) / 16.7;
-        Debug.meter(expected, {panel: Update.panels.meter});
-    },
+        Debug.meter(expected, {panel: this.panels.meter});
+    }
 
     /**
      * update percentage panel
+     * @param {number} other time
      */
-    _debugPercent: function(other)
+    _debugPercent(other)
     {
-        var change = Update.percentageList['Other'];
+        var change = this.percentageList['Other'];
         change.amounts[change.current++] = other;
-        change.current %= Update.rollingAverage;
+        change.current %= this.rollingAverage;
         var updates = [], all = 0;
-        for (var name in Update.percentageList)
+        for (var name in this.percentageList)
         {
-            var change = Update.percentageList[name];
+            var change = this.percentageList[name];
             var total = 0;
             for (var i = 0; i < change.amounts.length; i++)
             {
@@ -420,29 +432,11 @@ var Update = {
         }
         var update = updates[0];
         result += update.name + ': ' + Math.round(update.total / all * 100) + '%<br>';
-        debugOne(result, {panel: Update.panels.percent});
+        Debug.one(result, {panel: this.panels.percent});
     }
-};
-
-// add support for AMD (Asynchronous Module Definition) libraries such as require.js.
-if (typeof define === 'function' && define.amd)
-{
-    define(function()
-    {
-        return {
-            Update: Update
-        };
-    });
 }
 
-// add support for CommonJS libraries such as browserify.
-if (typeof exports !== 'undefined')
-{
-    module.exports = Update;
-}
+module.exports = new Update();
 
-// define globally in case AMD is not available or available but not used
-if (typeof window !== 'undefined')
-{
-    window.Update = Update;
-}
+// for eslint
+/* globals performance, requestAnimationFrame, setTimeout, window, document */
